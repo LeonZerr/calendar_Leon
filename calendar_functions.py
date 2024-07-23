5164df8a100eb36e40e389618ebdd4e330ad4f65
LeonZerr <leonz99@web.de>
2024-07-23 15:34:47 +0200

    updated

--- calendar_functions.py
+++ calendar_functions.py
@@ -4,18 +4,15 @@ year = datetime.datetime.now().year
 month = datetime.datetime.now().month
 import json
 
-def load_events():
-    with open('events.json') as f:
+def load_entries():
+    with open('entries.json') as f:
         data = json.load(f) 
-    return data['events']
+    return data['entries']
 
-def save_events(events):
-    with open('events.json','w') as file:
-        json.dump({"events":events},file,indent=4)
+def save_entries(entries):
+    with open('entries.json','w') as file:
+        json.dump({"entries":entries},file,indent=4)
 
-
-
-    
 def display_current_month():
     print()
     print(calendar.month(year,month))
@@ -24,7 +21,14 @@ def display_current_year():
     print()
     print(calendar.calendar(year))
 
-def add_an_event(datetime,description):
-       datetime = input()
+def add_entry(type,name_or_description,date):
+    if type == 'birthday':
+        entries.birthday.append("name_or_description":name_or_description,"date":date)
+    elif type == 'meeting':
+        entries.meeting.append("name_or_description":name_or_description,"date":date)
+    elif type == 'event':
+        entries.event.append("name_or_description":name_or_description,"date":date)       
+    save_entries(entries)
+
     
     
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import calendar
 import datetime
 from calendar_functions import *
 
-
 year = datetime.datetime.now().year
 month = datetime.datetime.now().month
 
@@ -10,15 +9,13 @@ def main():
     while True:
         print("\n1. Show the current month.")
         print("2. Show the current year.")
-        print("3. Add an event.")
-        print("4. Add a birthday.")
-        print("5. Change an event.")
-        print("6. Delete an event.")
-        print("7. Delete a birthday.")
+        print("3. Add entry.")
+        print("4. Change entry.")
+        print("5. Delete entry.")
+        print("6. Show specific entry.")
+        print("7. Show all birthdays.")
         print("8. Show all events.")
-        print("9. Show all birthdays.")
-        print("10. Show birthday of a specific person.")
-        print("11. Show specific event.")
+        print("9. Show all meetings.")
         print("q. Exit")
         
         option = input("\nSelect an option: ")
@@ -26,6 +23,11 @@ def main():
           display_current_month()
         elif option == '2':
           display_current_year()  
+        elif option == '3':
+          type = input('Enter a type(birthday, event or meeting): ')
+          name_or_description = input('Enter the name(if birthday) or description(event or meeting): ')
+          date = float(input('Enter the date: '))
+          add_entry(type,name_or_description,date)   
         elif option == 'q':
             break
         else:
