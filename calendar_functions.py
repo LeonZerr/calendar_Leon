bef82c33847c1a5618e5be00ef54d9c01511f013
LeonZerr <leonz99@web.de>
2024-07-26 11:29:01 +0200

    files updated

--- calendar_functions.py
+++ calendar_functions.py
@@ -5,30 +5,54 @@ month = datetime.datetime.now().month
 import json
 
 def load_entries():
-    with open('entries.json') as f:
+    with open('/home/dci-student/Desktop/Python_projects/calendar_Leon/entries.json') as f:
         data = json.load(f) 
     return data['entries']
 
-def save_entries(entries):
-    with open('entries.json','w') as file:
+def save_entry(entries):
+    with open('/home/dci-student/Desktop/Python_projects/calendar_Leon/entries.json','w') as file:
         json.dump({"entries":entries},file,indent=4)
+        print("Entry saved!")
 
-def display_current_month():
+def display_current_month():                                       
     print()
     print(calendar.month(year,month))
 
-def display_current_year():
+def display_current_year():                                        
     print()
     print(calendar.calendar(year))
 
-def add_entry(type,name_or_description,date):
-    if type == 'birthday':
-        entries.birthday.append("name_or_description":name_or_description,"date":date)
-    elif type == 'meeting':
-        entries.meeting.append("name_or_description":name_or_description,"date":date)
-    elif type == 'event':
-        entries.event.append("name_or_description":name_or_description,"date":date)       
-    save_entries(entries)
+def add_entry(data,type,name_or_description,date):
+    if type == 'B':                                    
+        data["birthdays"].update({name_or_description : date})               
+    elif type == 'M':
+        data["meetings"].update({name_or_description : date})
+    elif type == 'E':
+        data["events"].update({name_or_description : date})   
+    save_entry(data)                                                            
 
+def get_entry(data,type,name_or_description):
+    if type == 'B': 
+        dict_birth = data["birthdays"]
+        if name_or_description in dict_birth:
+            return dict_birth[name_or_description]
+        else:
+            return 'No entry found.'
+    if type == 'M':
+        dict_meet = data["meetings"] 
+        if name_or_description in dict_meet:
+            return dict_meet[name_or_description]
+        else:
+            return 'No entry found.'
+    if type == 'E': 
+        dict_event = data["events"]
+        if name_or_description in dict_event:
+            return dict_event[name_or_description]
+        else:
+            return 'No entry found.'
+
+def display_entry(data,type,name_or_description):
+    entry = get_entry(data,type,name_or_description)
+    print(f'{name_or_description} : {entry}')
     
     
--- main.py
+++ main.py
@@ -4,15 +4,16 @@ from calendar_functions import *
 
 year = datetime.datetime.now().year
 month = datetime.datetime.now().month
+data = load_entries()
 
 def main():
     while True:
         print("\n1. Show the current month.")
         print("2. Show the current year.")
         print("3. Add entry.")
-        print("4. Change entry.")
+        print("4. Show specific entry.")
         print("5. Delete entry.")
-        print("6. Show specific entry.")
+        print("6. Change entry.")
         print("7. Show all birthdays.")
         print("8. Show all events.")
         print("9. Show all meetings.")
@@ -24,10 +25,17 @@ def main():
         elif option == '2':
           display_current_year()  
         elif option == '3':
-          type = input('Enter a type(birthday, event or meeting): ')
-          name_or_description = input('Enter the name(if birthday) or description(event or meeting): ')
-          date = float(input('Enter the date: '))
-          add_entry(type,name_or_description,date)   
+          type = input('Choose type(B = birthday, E = event or M = meeting): ')
+          name_or_description = input('Enter the name(if birthday) or description(for an event or meeting): ')
+          date = (input('Enter the date: '))
+          add_entry(data,type,name_or_description,date)   
+        elif option == '4':
+          type = input('Choose type of entry you are searching for(B = birthday, E = event or M = meeting): ')
+          name_or_description = (input('Enter the name/description of the entry: '))
+          display_entry(data,type,name_or_description)
+        
+        
+        
         elif option == 'q':
             break
         else:
